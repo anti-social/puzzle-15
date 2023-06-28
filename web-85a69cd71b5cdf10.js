2b383c15e89c0517ac69d07cd5e2d76c3575576e
Alexander Koval <kovalidis@gmail.com>
2023-06-28 12:39:13 +0300

    Update

--- web-85a69cd71b5cdf10.js
+++ web-85a69cd71b5cdf10.js
@@ -755,7 +755,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-fc28a0a5e238ed50_bg.wasm', import.meta.url);
+        input = new URL('web-85a69cd71b5cdf10_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
