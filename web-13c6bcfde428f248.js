ba838bb754c0f389778781dccc31fefa09fd7928
Alexander Koval <kovalidis@gmail.com>
2023-07-26 17:44:52 +0300

    Update

--- web-13c6bcfde428f248.js
+++ web-13c6bcfde428f248.js
@@ -785,7 +785,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-4e62d73c6683129f_bg.wasm', import.meta.url);
+        input = new URL('web-13c6bcfde428f248_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
